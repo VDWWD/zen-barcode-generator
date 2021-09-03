e4e5ab34cb8f6881fb292422de57b26ad48aa146
VDWWD <32164874+VDWWD@users.noreply.github.com>
2021-09-03 15:23:50 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zen.Barcode;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //create the barcode
+            var barcode = new ZenBarcode.BarcodeData()
+            {
+                type = BarcodeSymbology.CodeQr,
+                height = 200,
+                value = "van der Waal Webdesign - www.vanderwaal.eu"
+            };
+
+            //create the model
+            var model = new Models.HomeViewModel()
+            {
+                barcode_base64 = "data:image/png;base64," + Convert.ToBase64String(barcode.Generate())
+            };
+
+            return View(model);
+        }
+    }
+}
